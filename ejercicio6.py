4a1a074180d768a0b1e254e13f1d0b218892755a
elenapelaez101 <elena.pelaezsanchez@usp.ceu.es>
2022-10-27 19:23:58 +0200

    comit

---
+++ ejercicio6.py
@@ -0,0 +1,8 @@
+"""Realiza el  código para calcular el determinante de una matriz cuadrada de [5 x 5],
+ regla de Sarrus de forma recursiva y de forma iterativa"""
+
+def creacionmatriz(fil1,fil2,fil3)
+
+
+
+
