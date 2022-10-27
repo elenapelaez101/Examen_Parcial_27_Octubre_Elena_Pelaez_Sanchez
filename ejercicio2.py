7e47eadd0c3120054603a6d7a1328cba5f48bca4
elenapelaez101 <elena.pelaezsanchez@usp.ceu.es>
2022-10-27 19:10:45 +0200

    cambios

--- ejercicio2.py
+++ ejercicio2.py
@@ -10,3 +10,14 @@ Organizar la lista mediante el método de ordenamiento merge sort
 Dada la lista anterior y un valor 145 devolver el índice de 145 en la lista si 145 está en la lista, y −1 si 145 no está en la lista
 """
 
+lista= [18,50,210,80,145,333,70,30]
+
+
+for i in l:
+    if l> 30:
+        break
+    elif l%10 ==0 and l< 200
+    print(l)
+
+
+
